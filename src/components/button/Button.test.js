8dc7c5439761c0ed9fdeed7503f35c799be56d2f
Joe Van Gundy <joevangundy@joes-mbp.lan>
2022-06-15 16:12:57 -0500

    working with old way

--- src/components/button/Button.test.js
+++ src/components/button/Button.test.js
@@ -1,6 +1,6 @@
 import { render, getByText, fireEvent } from "@testing-library/react";
 import React from "react";
-import Button from "components/Button";
+import Button from "components/button/Button";
 
 describe("Button", () => {
   test("should display text", () => {
