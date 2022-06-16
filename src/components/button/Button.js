e9db6d46edcdc08f9d7f0568563651dd9dcd997f
Joe Van Gundy <joevangundy@joes-mbp.lan>
2022-06-16 12:40:10 -0500

    working storybook and plot

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Button({ text, onClick, color }) {
   return (
@@ -20,7 +20,7 @@ Button.propTypes = {
   /**
    * How large should the button be?
    */
-  size: PropTypes.oneOf(["small", "medium", "large"]),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
   /**
    * Button contents
    */
@@ -28,13 +28,13 @@ Button.propTypes = {
   /**
    * Optional click handler
    */
-  onClick: PropTypes.func,
+  onClick: PropTypes.func
 };
 
 Button.defaultProps = {
   color: null,
   primary: false,
-  size: "medium",
+  size: 'medium',
   onClick: undefined,
-  color: "orange",
+  color: 'orange'
 };
--- src/components/button/Button.test.js
+++ src/components/button/Button.test.js
@@ -1,18 +1,16 @@
-import { render, getByText, fireEvent } from "@testing-library/react";
-import React from "react";
-import Button from "components/button/Button";
+import { render, getByText, fireEvent } from '@testing-library/react';
+import React from 'react';
+import Button from 'components/button/Button';
 
-describe("Button", () => {
-  test("should display text", () => {
-    const { container } = render(<Button text='We Salute You!' />);
-    getByText(container, "We Salute You!");
+describe('Button', () => {
+  test('should display text', () => {
+    const { container } = render(<Button text="We Salute You!" />);
+    getByText(container, 'We Salute You!');
   });
 
-  test("should handle click events", () => {
+  test('should handle click events', () => {
     const onClickMock = jest.fn();
-    const { container } = render(
-      <Button text='Click me, maybe?' onClick={onClickMock} />
-    );
+    const { container } = render(<Button text="Click me, maybe?" onClick={onClickMock} />);
     const component = container.firstChild;
     fireEvent.click(component);
     expect(onClickMock).toBeCalled();
