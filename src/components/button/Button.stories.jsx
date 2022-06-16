57dd2b68b2fa17fc109041a1deb66a6a8620d601
Joe Van Gundy <joevangundy@joes-mbp.lan>
2022-06-16 08:30:10 -0500

    working snapshot tests

--- src/components/button/Button.stories.jsx
+++ src/components/button/Button.stories.jsx
@@ -7,9 +7,9 @@ export default {
   title: "Tingle/Button",
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  // argTypes: {
+  //   backgroundColor: { control: "color" },
+  // },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -18,7 +18,7 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
+  // primary: true,
   text: "Button",
 };
 
