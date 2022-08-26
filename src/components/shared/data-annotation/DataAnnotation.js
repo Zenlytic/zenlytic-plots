1fbc2de62b428aa307e0520963a7947526dbfb72
Paul Blankley <paul@zenlytic.com>
2022-08-25 21:01:34 -0600

    Linting errors

--- src/components/shared/data-annotation/DataAnnotation.js
+++ src/components/shared/data-annotation/DataAnnotation.js
@@ -1,12 +1,13 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 
-const DataAnnotation = ({ x, y, stroke, value, formatter = () => {} }) => {
+function DataAnnotation({ x, y, stroke, value, formatter }) {
   return (
     <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">
       {formatter(value)}
     </text>
   );
-};
+}
 
 export default DataAnnotation;
