2af7b88780a877d7ff6871f5a1b50160d1b5eac6
Nicholas Morrow <njmorrow95@gmail.com>
2022-12-02 16:14:52 -0600

    Use theme file font size for Data Annotation.

--- src/components/shared/data-annotation/DataAnnotation.js
+++ src/components/shared/data-annotation/DataAnnotation.js
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import fontSizes from '../../../constants/fontSizes';
 
 function DataAnnotation({ x, y, stroke, value, valueFormatter }) {
   return (
-    <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">
+    <text x={x} y={y} dy={-4} fill={stroke} fontSize={fontSizes['2sx']} textAnchor="middle">
       {valueFormatter(value)}
     </text>
   );
