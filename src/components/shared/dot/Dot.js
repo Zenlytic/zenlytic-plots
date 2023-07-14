0903aaa8f4487495ae10897a33e591dc90c5a2d7
Nicholas Morrow <njmorrow95@gmail.com>
2023-07-14 10:31:03 -0500

    Fix

--- src/components/shared/dot/Dot.js
+++ src/components/shared/dot/Dot.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function Dot({ color, strokeWidth = 1, radius = '2', ...rest }) {
-  return <circle {...rest} strokeWidth={strokeWidth} fill={color} stroke="white" r={radius} />;
+function Dot({ cx, cy, color, strokeWidth = 1, radius = '2', ...rest }) {
+  if (cx === null || cy === null) {
+    return null;
+  }
+  return (
+    <circle
+      {...rest}
+      cx={cx}
+      cy={cy}
+      strokeWidth={strokeWidth}
+      fill={color}
+      stroke="white"
+      r={radius}
+    />
+  );
 }
 
 export default Dot;
